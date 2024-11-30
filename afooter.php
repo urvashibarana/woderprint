1135be52f4656dee75cecf485a23ab075de20959
urvashibarana <ranaurvashiba21@gmail.com>
2024-11-30 21:12:20 +0530

    Add files via upload

---
+++ afooter.php
@@ -0,0 +1,46 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Footer Start -->
+ <div class="container-fluid bg-dark bg-footer text-light py-5">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-5 col-md-5">
+                    <h4 class="text" style="color: var(--primary);">Get In Touch</h4>
+                    <hr class="w-25 text-secondary mb-4" style="opacity: 1;">
+                    <h5 class="mb-4" style="color: var(--primary);">Wonderprint</h5>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-secondary me-3"></i>26,1st Floor,Matrushree Complex,Above Central Bank,Joravarnagar</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-secondary me-3"></i>wonderprint@gmail.com</p>
+                    <p class="mb-0"><i class="fa fa-phone-alt text-secondary me-3"></i>95674 89012</p>
+                </div>
+                <div class="col-lg-7 col-md-7">
+                    <h4 class="" style="color: var(--primary);">Quick Links</h4>
+                    <hr class="w-25 text-secondary mb-4" style="opacity: 1;">
+                    <div class="d-flex flex-column justify-content-start">
+                       
+                          <a class="text-light mb-2" href="ahome.php"><i class="fa fa-angle-right me-2"></i>Home</a>
+                          <a class="text-light mb-2" href="product_view.php"><i class="fa fa-angle-right me-2"></i>Printproducts</a>
+                          <a class="text-light mb-2" href="order_view.php"><i class="fa fa-angle-right me-2"></i>Orders</a>
+                          <a class="text-light mb-2" href="registration_view.php"><i class="fa fa-angle-right me-2"></i>Users</a>
+                        <a class="text-light mb-2" href="feedback_view.php"><i class="fa fa-angle-right me-2"></i>Feedback</a>
+                        <a class="text-light mb-2" href="logout.php"><i class="fa fa-angle-right me-2"></i>Logout</a>  
+                </div>
+                    </div>
+                    </div> 
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid bg-primary text-dark py-4">
+        <div class="container">
+            <div class="row g-0">
+                <div class="col-md-6 text-center text-md-start">
+                    <p class="mb-md-0">Copyright &copy; <a class="text-dark fw-bold" href="#">Wonderprint</a>. All Rights Reserved.</p>
+                </div>
+                <div class="col-md-6 text-center text-md-end">
+                    <p class="mb-0">Designed by <a class="text-dark fw-bold" href="https://htmlcodex.com">Urvashiba Rana</a></p>
+                </div>
+            </div>
+        </div>
+    </div>    <!-- Footer End -->
+                    </html>
